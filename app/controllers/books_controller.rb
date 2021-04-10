8dd6ab66dbd7f1327c1b60febd474a1047e0a07e
Fikutaro <nomio9029@gmail.com>
2021-04-10 12:03:08 +0000

    [Finish]3-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,9 +13,13 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    if book.save
-       redirect_to books_path, notice: 'Book was succesfully created.'
+    @books = Book.all
+    @book = Book.new(book_params)
+    if @book.save
+       flash[:notice] = 'Book was successfully created.'
+       redirect_to book_path(@book.id)
+    else
+       render "index"
     end
   end
   
@@ -24,9 +28,13 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+       flash[:notice] = 'Book was successfully updated.'
+       redirect_to book_path(@book.id)
+    else
+      render :edit
+    end  
   end
     
   def destroy
